dcc96a4beeb82b1fa1f04b8c5bcd30392602eb97
Einat Sela <einat.sela2022@gmail.com~>
2022-03-14 14:19:32 +0200

    singup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { SingupFormComponent } from './singup-form/singup-form.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/singup-form/singup-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-singup-form',
+  templateUrl: './singup-form.component.html',
+  styleUrls: ['./singup-form.component.css']
+})
+export class SingupFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addUser(firstname:string, lastname:string, age:number, address:string, 
+    workplace:string, username:string, password:string, passwordconfirmation:string)
+    {
+
+    }
+
+}
