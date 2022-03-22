0ecb1194d1f6c21e22efe83b6c2766ef8d5f49e6
yael <yael@example.com>
2022-03-22 14:24:58 +0200

    like

--- src/app/services/converter.service.ts
+++ src/app/services/converter.service.ts
@@ -3,20 +3,21 @@ import { AcEntity } from 'angular-cesium';
 import { Post } from '../models/Post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConverterService {
+  constructor() {}
 
-  constructor() { }
-
-  postToAcEntity(post:Post) : AcEntity{
-    console.log(post);
+  postToAcEntity(post: Post): AcEntity {
     return {
-      id : post.id,
-      description : post.description,
-      imageSorce : post.imageSorce,
-      location: new Cesium.Cartesian3(post.x_Position, post.y_Position, post.z_Position)
+      id: post.id,
+      description: post.description,
+      imageSorce: post.imageSorce,
+      location: new Cesium.Cartesian3(
+        post.x_Position,
+        post.y_Position,
+        post.z_Position
+      ),
     };
-
   }
 }
